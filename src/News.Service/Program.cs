aa8f4f409e793a32397423416bc7178e03c9349d
Vladislav Borovikov <vborovikov@live.ru>
2023-08-21 14:09:45 +0500

    created Newsmaker project

---
+++ src/News.Service/Program.cs
@@ -0,0 +1,16 @@
+namespace News.Service;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        IHost host = Host.CreateDefaultBuilder(args)
+            .ConfigureServices(services =>
+            {
+                services.AddHostedService<Worker>();
+            })
+            .Build();
+
+        host.Run();
+    }
+}
