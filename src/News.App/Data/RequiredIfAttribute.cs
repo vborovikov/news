2e91ca6b556be680e3421485c6e5dbf6142c4550
Vladislav Borovikov <vborovikov@live.ru>
2023-08-22 08:08:13 +0500

    added client-side validation for RequiredIf

--- src/News.App/Data/RequiredIfAttribute.cs
+++ src/News.App/Data/RequiredIfAttribute.cs
@@ -3,22 +3,24 @@
 using System;
 using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
 
 [AttributeUsage(AttributeTargets.Property, AllowMultiple = true)]
-public class RequiredIfAttribute : ValidationAttribute
+public class RequiredIfAttribute : ValidationAttribute, IClientModelValidator
 {
-    private readonly string propertyName;
+    private readonly string otherPropertyName;
     private readonly object? desiredValue;
 
-    public RequiredIfAttribute(string propertyName, Type propertyType, string value)
-        : this(propertyName, TypeDescriptor.GetConverter(propertyType).ConvertFromInvariantString(value))
+    public RequiredIfAttribute(string otherPropertyName, Type otherPropertyType, string otherPropertyValue)
+        : this(otherPropertyName, TypeDescriptor.GetConverter(otherPropertyType).ConvertFromInvariantString(otherPropertyValue))
     {
     }
 
-    public RequiredIfAttribute(string propertyName, object? value)
+    public RequiredIfAttribute(string otherPropertyName, object? otherPropertyValue)
     {
-        this.propertyName = propertyName;
-        this.desiredValue = value;
+        this.otherPropertyName = otherPropertyName;
+        this.desiredValue = otherPropertyValue;
     }
 
     public override bool IsValid(object? value)
@@ -41,7 +43,7 @@ public class RequiredIfAttribute : ValidationAttribute
     {
         var instance = validationContext.ObjectInstance;
         var type = instance.GetType();
-        var currentValue = type.GetProperty(this.propertyName)?.GetValue(instance, null);
+        var currentValue = type.GetProperty(this.otherPropertyName)?.GetValue(instance, null);
 
         if (Object.ReferenceEquals(currentValue, this.desiredValue) ||
             (currentValue?.Equals(this.desiredValue) == true))
@@ -50,5 +52,42 @@ public class RequiredIfAttribute : ValidationAttribute
         }
 
         return ValidationResult.Success;
+    }
+
+    public void AddValidation(ClientModelValidationContext context)
+    {
+        var thisFieldName = GetFieldName(context.ModelMetadata);
+        var otherFieldName = GetFieldName(context.ModelMetadata.ContainerMetadata?.Properties[this.otherPropertyName]) ?? this.otherPropertyName;
+
+        context.Attributes.TryAdd("data-val", "true");
+        context.Attributes.TryAdd("data-val-requiredif", $"The {thisFieldName} field is required if {otherFieldName} field is not set.");
+        context.Attributes.TryAdd("data-val-requiredif-value", this.desiredValue?.ToString() ?? "");
+
+        var otherPropertyInputName = this.otherPropertyName;
+
+        var thisPropertyName = context.ModelMetadata.PropertyName;
+        var thisPropertyInputName = context.Attributes["name"];
+        if (thisPropertyName is not null && thisPropertyInputName is not null)
+        {
+            otherPropertyInputName = ReplaceLast(thisPropertyInputName, thisPropertyName, this.otherPropertyName);
+        }
+
+        // let's hope the input name is correct
+        context.Attributes.TryAdd("data-val-requiredif-property", otherPropertyInputName);
+
+        static string? GetFieldName(ModelMetadata? modelMetadata)
+        {
+            return modelMetadata?.DisplayName ?? modelMetadata?.Name;
+        }
+
+        static string ReplaceLast(string source, string search, string replace)
+        {
+            int place = source.LastIndexOf(search);
+
+            if (place == -1)
+                return source;
+
+            return source.Remove(place, search.Length).Insert(place, replace);
+        }
     }
 }
