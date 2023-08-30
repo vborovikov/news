5b7c9e84cb59531d3a9259cb1d8b4fba5e44f3bc
Vladislav Borovikov <vborovikov@live.ru>
2023-08-30 20:13:55 +0500

    implemented export command

--- src/News.App/Data/DapperSupport.cs
+++ src/News.App/Data/DapperSupport.cs
@@ -8,18 +8,23 @@ using Dapper;
 
 static class DapperSupport
 {
-    public static async Task<T?> QueryJsonAsync<T>(this DbConnection cnn, string sql, object? param = null)
+    public static async Task<string> QueryTextAsync(this DbConnection cnn, string sql, object? param = null)
     {
         await using var reader = await cnn.ExecuteReaderAsync(sql, param);
-        var json = new StringBuilder(1024 * 3);
+        var text = new StringBuilder(1024 * 3);
         while (await reader.ReadAsync())
         {
-            json.Append(reader.GetString(0));
+            text.Append(reader.GetString(0));
         }
+        return text.ToString();
+    }
 
+    public static async Task<T?> QueryJsonAsync<T>(this DbConnection cnn, string sql, object? param = null)
+    {
+        var json = await cnn.QueryTextAsync(sql, param);
         if (json.Length == 0)
             return default;
 
-        return JsonSerializer.Deserialize<T>(json.ToString());
+        return JsonSerializer.Deserialize<T>(json);
     }
 }
--- src/News.App/Pages/Export.cshtml.cs
+++ src/News.App/Pages/Export.cshtml.cs
@@ -1,11 +1,62 @@
 namespace News.App.Pages;
 
+using System.Data.Common;
+using System.Text;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using News.App.Data;
 
+[Authorize]
 public class ExportModel : PageModel
 {
-    public void OnGet()
+    private readonly UserManager<AppUser> userManager;
+    private readonly DbDataSource db;
+    private readonly ILogger<ExportModel> log;
+
+    public ExportModel(UserManager<AppUser> userManager, DbDataSource db, ILogger<ExportModel> log)
     {
+        this.userManager = userManager;
+        this.db = db;
+        this.log = log;
+    }
+
+    public async Task<IActionResult> OnGetAsync(string? format = null, CancellationToken cancellationToken = default)
+    {
+        if (string.IsNullOrWhiteSpace(format))
+        {
+            return Page();
+        }
+        var userIdStr = this.userManager.GetUserId(this.User);
+        if (!Guid.TryParse(userIdStr, out var userId))
+        {
+            return Forbid();
+        }
+
+        await using var cnn = await this.db.OpenConnectionAsync(cancellationToken);
+        // query channels and feeds in XML format
+        var xml = await cnn.QueryTextAsync(
+            """
+            select
+            	(select ui.UserName as [title]
+            	 from asp.Users ui
+            	 where ui.Id = u.Id
+            	 for xml raw('head'), type, elements),
+            	(select uc.Name as [title], uc.Slug as [text], 
+            		(select isnull(uf.Title, f.Title) as [title], uf.Slug as [text], f.Source as [xmlUrl], f.Link as [htmlUrl]
+            		 from rss.UserFeeds uf
+            		 inner join rss.Feeds f on uf.FeedId = f.Id
+            		 where uc.Id = uf.ChannelId
+            		 for xml raw('outline'), type)
+            	from rss.UserChannels uc
+            	where uc.UserId = u.Id
+            	for xml raw('outline'), type) as [body]
+            from asp.Users u
+            where u.Id = @UserId
+            for xml raw('opml'), type;
+            """, new { UserId = userId });
+
+        return File(Encoding.UTF8.GetBytes(xml), "text/x-opml", "feeds.opml");
     }
 }
