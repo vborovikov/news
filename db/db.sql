552dd3fd916ecd727eeee0d823ba0c279f750054
Vladislav Borovikov <vborovikov@live.ru>
2023-09-07 03:03:16 +0500

    refactored asp db schema

--- db/db.sql
+++ db/db.sql
@@ -10,7 +10,7 @@ end;
 if @@Error = 3702
    RaisError('Cannot delete the database because of the open connections.', 127, 127) with nowait, log;
 
-create database News;
+create database News collate Latin1_General_100_CI_AS;
 go
 
 use News;
@@ -33,76 +33,75 @@ set quoted_identifier on;
 go
 
 create table [asp].[Roles] (
-    [Id] uniqueidentifier not null primary key,
-    [Name] nvarchar(256) null,
-    [NormalizedName] nvarchar(256) null index [UX_Roles_NormalizedName] unique where [NormalizedName] is not null,
-    [ConcurrencyStamp] nvarchar(max) null
+    [Id] uniqueidentifier not null primary key clustered,
+    [Name] varchar(128) not null,
+    [NormalizedName] varchar(128) not null index [UX_Roles_NormalizedName] unique,
+    [ConcurrencyStamp] varchar(128) not null
 );
 go
 
 create table [asp].[Users] (
-    [Id] uniqueidentifier NOT NULL primary key,
-    [UserName] nvarchar(256) NULL,
-    [NormalizedUserName] nvarchar(256) NULL index [UX_Users_NormalizedUserName] unique where [NormalizedUserName] is not null,
-    [Email] nvarchar(256) NULL,
-    [NormalizedEmail] nvarchar(256) null index [IX_Users_NormalizedEmail],
-    [EmailConfirmed] bit NOT NULL,
-    [PasswordHash] nvarchar(max) NULL,
-    [SecurityStamp] nvarchar(max) NULL,
-    [ConcurrencyStamp] nvarchar(max) NULL,
-    [PhoneNumber] nvarchar(max) NULL,
-    [PhoneNumberConfirmed] bit NOT NULL,
-    [TwoFactorEnabled] bit NOT NULL,
-    [LockoutEnd] datetimeoffset NULL,
-    [LockoutEnabled] bit NOT NULL,
-    [AccessFailedCount] int NOT NULL
+    [Id] uniqueidentifier not null primary key clustered,
+    [UserName] nvarchar(256) not null,
+    [NormalizedUserName] nvarchar(256) not null index [UX_Users_NormalizedUserName] unique,
+    [Email] varchar(256) not null,
+    [NormalizedEmail] varchar(256) not null index [IX_Users_NormalizedEmail] unique,
+    [EmailConfirmed] bit not null,
+    [PasswordHash] varchar(128) not null,
+    [SecurityStamp] varchar(128) not null,
+    [ConcurrencyStamp] varchar(128) not null,
+    [PhoneNumber] nvarchar(128) null,
+    [PhoneNumberConfirmed] bit not null,
+    [TwoFactorEnabled] bit not null,
+    [LockoutEnd] datetimeoffset null,
+    [LockoutEnabled] bit not null,
+    [AccessFailedCount] int not null
 );
 go
 
 create table [asp].[RoleClaims] (
     [Id] int not null identity primary key,
-    [RoleId] uniqueidentifier not null index [IX_RoleClaims_RoleId],
-    [ClaimType] nvarchar(max) NULL,
-    [ClaimValue] nvarchar(max) NULL,
-    constraint [FK_RoleClaims_Roles_RoleId] foreign key ([RoleId]) references [asp].[Roles] ([Id]) on delete cascade
+    [RoleId] uniqueidentifier not null foreign key references [asp].[Roles] ([Id]) on delete cascade,
+    [ClaimType] varchar(128) null,
+    [ClaimValue] varchar(128) null,
+    index [IXC_RoleClaims] clustered ([RoleId], [Id])
 );
 go
 
 create table [asp].[UserClaims] (
     [Id] int not null identity primary key,
-    [UserId] uniqueidentifier not null index [IX_UserClaims_UserId],
-    [ClaimType] nvarchar(max) NULL,
-    [ClaimValue] nvarchar(max) NULL,
-    constraint [FK_UserClaims_Users_UserId] foreign key ([UserId]) references [asp].[Users] ([Id]) on delete cascade
+    [UserId] uniqueidentifier not null foreign key references [asp].[Users] ([Id]) on delete cascade,
+    [ClaimType] varchar(128) null,
+    [ClaimValue] varchar(128) null,
+    index [IXC_UserClaims] clustered ([UserId], [Id])
 );
 go
 
 create table [asp].[UserLogins] (
-    [LoginProvider] nvarchar(450) NOT NULL,
-    [ProviderKey] nvarchar(450) NOT NULL,
-    [ProviderDisplayName] nvarchar(max) NULL,
-    [UserId] uniqueidentifier not null index [IX_UserLogins_UserId],
+    [LoginProvider] varchar(128) not null,
+    [ProviderKey] varchar(128) not null,
+    [ProviderDisplayName] nvarchar(128) null,
+    [UserId] uniqueidentifier not null foreign key references [asp].[Users] ([Id]) on delete cascade,
     constraint [PK_UserLogins] primary key ([LoginProvider], [ProviderKey]),
-    constraint [FK_UserLogins_Users_UserId] foreign key ([UserId]) references [asp].[Users] ([Id]) on delete cascade
+    index [IXC_UserLogins] clustered ([UserId], [LoginProvider], [ProviderKey])
 );
 go
 
 create table [asp].[UserRoles] (
-    [UserId] uniqueidentifier not null,
-    [RoleId] uniqueidentifier not null index [IX_UserRoles_RoleId],
+    [UserId] uniqueidentifier not null foreign key references [asp].[Users] ([Id]) on delete cascade,
+    [RoleId] uniqueidentifier not null foreign key references [asp].[Roles] ([Id]) on delete cascade,
     constraint [PK_UserRoles] primary key ([UserId], [RoleId]),
-    constraint [FK_UserRoles_Roles_RoleId] foreign key ([RoleId]) references [asp].[Roles] ([Id]) on delete cascade,
-    constraint [FK_UserRoles_Users_UserId] foreign key ([UserId]) references [asp].[Users] ([Id]) on delete cascade
+    index [IXC_UserRoles] clustered ([UserId], [RoleId])
 );
 go
 
 create table [asp].[UserTokens] (
-    [UserId] uniqueidentifier NOT NULL,
-    [LoginProvider] nvarchar(450) NOT NULL,
-    [Name] nvarchar(450) NOT NULL,
-    [Value] nvarchar(max) NULL,
+    [UserId] uniqueidentifier not null foreign key references [asp].[Users] ([Id]) on delete cascade,
+    [LoginProvider] varchar(128) not null,
+    [Name] varchar(128) not null,
+    [Value] varchar(128) null,
     constraint [PK_UserTokens] primary key ([UserId], [LoginProvider], [Name]),
-    constraint [FK_UserTokens_Users_UserId] foreign key ([UserId]) references [asp].[Users] ([Id]) on delete cascade
+    index [IXC_UserTokens] clustered ([UserId], [LoginProvider], [Name])
 );
 go
 
