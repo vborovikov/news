33b8371b989f8fefd86c1032a618106889cd63d3
Vladislav Borovikov <vborovikov@live.ru>
2023-09-09 04:26:51 +0500

    updated scripts

--- db/db_users.sql
+++ db/db_users.sql
@@ -1,12 +1,7 @@
--- IIS app user to access the DB server
-use master;
-go
-
-create login [IIS APPPOOL\news] from windows with default_database=master;
-go
-
 use News;
 go
+
+-- IIS user
 
 create user [IIS AppPool\news] for login [IIS APPPOOL\news];
 go
@@ -55,4 +50,41 @@ go
 grant delete on schema::[dbo] to [IIS AppPool\news]
 go
 grant execute on schema::[dbo] to [IIS AppPool\news]
+go
+
+-- SVC user
+
+create user [NT AUTHORITY\LOCAL SERVICE] for login [NT AUTHORITY\LOCAL SERVICE];
+go
+alter user [NT AUTHORITY\LOCAL SERVICE] with default_schema=[rss];
+go
+
+-- read/write roles
+alter role [db_datareader] add member [NT AUTHORITY\LOCAL SERVICE];
+go
+alter role [db_datawriter] add member [NT AUTHORITY\LOCAL SERVICE];
+go
+
+-- [rss] operations
+grant insert on schema::[rss] to [NT AUTHORITY\LOCAL SERVICE];
+go
+grant select on schema::[rss] to [NT AUTHORITY\LOCAL SERVICE];
+go
+grant update on schema::[rss] to [NT AUTHORITY\LOCAL SERVICE];
+go
+grant delete on schema::[rss] to [NT AUTHORITY\LOCAL SERVICE];
+go
+grant execute on schema::[rss] to [NT AUTHORITY\LOCAL SERVICE];
+go
+
+-- [dbo] operations
+grant insert on schema::[dbo] to [NT AUTHORITY\LOCAL SERVICE];
+go
+grant select on schema::[dbo] to [NT AUTHORITY\LOCAL SERVICE];
+go
+grant update on schema::[dbo] to [NT AUTHORITY\LOCAL SERVICE];
+go
+grant delete on schema::[dbo] to [NT AUTHORITY\LOCAL SERVICE];
+go
+grant execute on schema::[dbo] to [NT AUTHORITY\LOCAL SERVICE];
 go
--- db/svc.sql
+++ db/svc.sql
@@ -13,11 +13,13 @@ go
 alter user [NT AUTHORITY\LOCAL SERVICE] with default_schema=[rss];
 go
 
+-- read/write roles
 alter role [db_datareader] add member [NT AUTHORITY\LOCAL SERVICE];
 go
 alter role [db_datawriter] add member [NT AUTHORITY\LOCAL SERVICE];
 go
 
+-- [rss] operations
 grant insert on schema::[rss] to [NT AUTHORITY\LOCAL SERVICE];
 go
 grant select on schema::[rss] to [NT AUTHORITY\LOCAL SERVICE];
@@ -29,6 +31,7 @@ go
 grant execute on schema::[rss] to [NT AUTHORITY\LOCAL SERVICE];
 go
 
+-- [dbo] operations
 grant insert on schema::[dbo] to [NT AUTHORITY\LOCAL SERVICE];
 go
 grant select on schema::[dbo] to [NT AUTHORITY\LOCAL SERVICE];
