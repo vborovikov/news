e2e78a67717909ce2e3ad5c634e85516f3615999
Vladislav Borovikov <vborovikov@live.ru>
2023-09-08 20:21:06 +0500

    updated Export page

--- src/News.App/Pages/Export.cshtml.cs
+++ src/News.App/Pages/Export.cshtml.cs
@@ -1,5 +1,6 @@
 namespace News.App.Pages;
 
+using System.ComponentModel.DataAnnotations;
 using System.Data.Common;
 using System.Text;
 using Microsoft.AspNetCore.Authorization;
@@ -20,12 +21,21 @@ public class ExportModel : PageModel
         this.userManager = userManager;
         this.db = db;
         this.log = log;
+        this.Input = new();
     }
 
-    public async Task<IActionResult> OnGetAsync(string? format = null, CancellationToken cancellationToken = default)
+    [BindProperty]
+    public InputModel Input { get; init; }
+
+    public void OnGet()
     {
-        if (string.IsNullOrWhiteSpace(format))
+    }
+
+    public async Task<IActionResult> OnPostAsync(CancellationToken cancellationToken = default)
+    {
+        if (!this.ModelState.IsValid || this.Input.Format != FeedExportFormat.Opml)
         {
+            this.ModelState.AddModelError("", "Format not supported yet");
             return Page();
         }
         var userIdStr = this.userManager.GetUserId(this.User);
@@ -34,29 +44,59 @@ public class ExportModel : PageModel
             return Forbid();
         }
 
-        await using var cnn = await this.db.OpenConnectionAsync(cancellationToken);
-        // query channels and feeds in XML format
-        var xml = await cnn.QueryTextAsync(
-            """
-            select
-            	(select ui.UserName as [title]
-            	 from asp.Users ui
-            	 where ui.Id = u.Id
-            	 for xml raw('head'), type, elements),
-            	(select uc.Name as [title], uc.Slug as [text], 
-            		(select isnull(uf.Title, f.Title) as [title], uf.Slug as [text], f.Source as [xmlUrl], f.Link as [htmlUrl]
-            		 from rss.UserFeeds uf
-            		 inner join rss.Feeds f on uf.FeedId = f.Id
-            		 where uc.Id = uf.ChannelId
-            		 for xml raw('outline'), type)
-            	from rss.UserChannels uc
-            	where uc.UserId = u.Id
-            	for xml raw('outline'), type) as [body]
-            from asp.Users u
-            where u.Id = @UserId
-            for xml raw('opml'), type;
-            """, new { UserId = userId });
+        try
+        {
+            await using var cnn = await this.db.OpenConnectionAsync(cancellationToken);
+            // query channels and feeds in XML format
+            var xml = await cnn.QueryTextAsync(
+                """
+                select
+                    (select ui.UserName as [title]
+                    from asp.Users ui
+                    where ui.Id = u.Id
+                    for xml raw('head'), type, elements),
+                    (select uc.Name as [title], uc.Slug as [text], 
+                        (select isnull(uf.Title, f.Title) as [title], uf.Slug as [text], f.Source as [xmlUrl], f.Link as [htmlUrl]
+                        from rss.UserFeeds uf
+                        inner join rss.Feeds f on uf.FeedId = f.Id
+                        where uc.Id = uf.ChannelId and (@IncludeBroken = 1 or f.Error is null)
+                        order by [title]
+                        for xml raw('outline'), type)
+                    from rss.UserChannels uc
+                    where uc.UserId = u.Id
+                    order by uc.Name
+                    for xml raw('outline'), type) as [body]
+                from asp.Users u
+                where u.Id = @UserId
+                for xml raw('opml'), type;
+                """, new { UserId = userId, this.Input.IncludeBroken });
 
-        return File(Encoding.UTF8.GetBytes(xml), "text/x-opml", "feeds.opml");
+            return File(Encoding.UTF8.GetBytes(xml), "text/x-opml", "feeds.opml");
+        }
+        catch (Exception x)
+        {
+            this.log.LogError(x, "Error exporting feeds");
+            this.ModelState.AddModelError("", x.Message);
+            return Page();
+        }
+    }
+
+    public enum FeedExportFormat
+    {
+        [Display(Name = "OPML")]
+        Opml,
+        [Display(Name = "JSON")]
+        Json,
+        [Display(Name = "Text")]
+        Text,
+    }
+
+    public record InputModel
+    {
+        [Required(AllowEmptyStrings = false), Display(Name = "Format")]
+        public FeedExportFormat Format { get; init; }
+
+        [Display(Name = "Include Broken")]
+        public bool IncludeBroken { get; init; }
     }
 }
