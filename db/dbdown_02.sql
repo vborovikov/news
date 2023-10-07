e559856af6c01fe77cac70f125a5fa7e928cc30a
Vladislav Borovikov <vborovikov@live.ru>
2023-10-07 08:17:52 +0500

    added safeguard columns

---
+++ db/dbdown_02.sql
@@ -0,0 +1,35 @@
+use News;
+go
+
+-- v0.4.4
+
+begin transaction;
+go
+
+alter view rss.AppPosts with schemabinding as
+    select 
+        up.UserId, p.FeedId, p.Id as PostId, up.IsRead, up.IsFavorite,
+        p.Link, p.Slug, p.Published, p.Title, p.Description, p.Content, p.Author
+    from rss.Posts p
+    left outer join rss.UserPosts up on up.PostId = p.Id;
+go
+
+alter view rss.AppFeeds with schemabinding as
+    select uf.UserId, uf.ChannelId, uf.FeedId, f.Source,
+        isnull(uf.Title, f.Title) as Title, uf.Slug, 
+        f.Description, f.Link, f.Updated, f.Error
+    from rss.UserFeeds uf
+    inner join rss.Feeds f on f.Id = uf.FeedId;
+go
+
+alter table rss.Posts drop
+	column SafeDescription,
+    column SafeContent;
+go;
+
+alter table rss.Feeds drop
+	column Safeguards;
+go
+
+commit transaction;
+go
---
+++ db/dbup_02.sql
@@ -0,0 +1,37 @@
+use News;
+go
+
+-- v0.4.4
+
+begin transaction;
+go
+
+alter table rss.Feeds add
+	Safeguards varchar(150) not null default 'OK';
+go    
+
+alter table rss.Posts add
+	SafeDescription nvarchar(max) null,
+    SafeContent nvarchar(max) null check (SafeContent != N'');
+go
+
+alter view rss.AppFeeds with schemabinding as
+    select uf.UserId, uf.ChannelId, uf.FeedId, f.Source,
+        isnull(uf.Title, f.Title) as Title, uf.Slug, 
+        f.Description, f.Link, f.Updated, f.Error, f.Safeguards
+    from rss.UserFeeds uf
+    inner join rss.Feeds f on f.Id = uf.FeedId;
+go
+
+alter view rss.AppPosts with schemabinding as
+    select 
+        up.UserId, p.FeedId, p.Id as PostId, up.IsRead, up.IsFavorite,
+        p.Link, p.Slug, p.Published, p.Title, p.Author, 
+        isnull(p.SafeDescription, p.Description) as Description, 
+        isnull(p.SafeContent, p.Content) as Content
+    from rss.Posts p
+    left outer join rss.UserPosts up on up.PostId = p.Id;
+go
+
+commit transaction;
+go
