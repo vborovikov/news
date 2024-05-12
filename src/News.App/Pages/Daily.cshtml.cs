4966715639c9f26d8dd499499d799e8c7c600ffb
Vladislav Borovikov <vborovikov@live.ru>
2024-05-12 14:51:19 +0500

    added search to Daily page

--- src/News.App/Pages/Daily.cshtml.cs
+++ src/News.App/Pages/Daily.cshtml.cs
@@ -5,6 +5,7 @@ using System.Globalization;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using News.App.Data;
+using Relay.InteractionModel;
 
 [Authorize]
 public class DailyModel : AppPageModel
@@ -24,8 +25,13 @@ public class DailyModel : AppPageModel
 
     public IEnumerable<ChannelSummary> Channels { get; private set; } = [];
 
-    public async Task OnGet([FromQuery] string? day = null, string? channel = null, CancellationToken cancellationToken = default)
+    public async Task<IActionResult> OnGet([FromQuery] PageRequest page, [FromQuery] string? day = null, CancellationToken cancellationToken = default)
     {
+        if (!string.IsNullOrWhiteSpace(page.Q))
+        {
+            return RedirectToPage("Search", new { q = page.Q });
+        }
+
         var maxPublished = DateTimeOffset.Now;
         var minPublished = maxPublished.AddDays(-1);
         if (day is string dateStr &&
@@ -33,49 +39,44 @@ public class DailyModel : AppPageModel
             date < DateTime.Today)
         {
             minPublished = new DateTimeOffset(date.Date);
-            maxPublished = new DateTimeOffset(date.Date.Add(new TimeSpan(23, 59, 59)));
+            maxPublished = new DateTimeOffset(date.Date.AddDays(1).AddSeconds(-1)); // -1 second to escape the current date
         }
         this.Date = maxPublished.Date;
 
         await using var cnn = await this.db.OpenConnectionAsync(cancellationToken);
-        if (string.IsNullOrWhiteSpace(channel))
-        {
-            this.Channels = await cnn.QueryJsonAsync<IEnumerable<ChannelSummary>>(
-                """
-                select uc.Id as ChannelId, uc.Name, uc.Slug,
-                    json_query((
-                        select rp.*
-                        from (
-                            select
-                                ap.PostId, ap.Title, ap.Published, ap.Description, 
-                                ap.Link, ap.Slug, ap.Author, ap.IsRead, ap.IsFavorite,
-                                af.Slug as FeedSlug, af.Title as FeedTitle,
-                                row_number() over (partition by af.FeedId order by ap.Published desc) as PostNumber
-                            from rss.AppFeeds af
-                            inner join rss.AppPosts ap on af.FeedId = ap.FeedId
-                            where af.UserId = @UserId and af.ChannelId = uc.Id and
-                                ap.Published >= @MinPublished and ap.Published <= @MaxPublished
-                        ) rp
-                        where rp.PostNumber <= 3
-                        order by rp.FeedTitle, rp.Published desc
-                        for json path
-                    )) as Posts
-                from rss.UserChannels uc
-                where uc.UserId = @UserId
-                order by uc.Name
-                for json path;
-                """,
-                new
-                {
-                    this.UserId,
-                    MinPublished = minPublished,
-                    MaxPublished = maxPublished,
-                }) ?? [];
-        }
-        else
-        {
+        this.Channels = await cnn.QueryJsonAsync<IEnumerable<ChannelSummary>>(
+            """
+            select uc.Id as ChannelId, uc.Name, uc.Slug,
+                json_query((
+                    select rp.*
+                    from (
+                        select
+                            ap.PostId, ap.Title, ap.Published, ap.Description, 
+                            ap.Link, ap.Slug, ap.Author, ap.IsRead, ap.IsFavorite,
+                            af.Slug as FeedSlug, af.Title as FeedTitle,
+                            row_number() over (partition by af.FeedId order by ap.Published desc) as PostNumber
+                        from rss.AppFeeds af
+                        inner join rss.AppPosts ap on af.FeedId = ap.FeedId
+                        where af.UserId = @UserId and af.ChannelId = uc.Id and
+                            ap.Published >= @MinPublished and ap.Published <= @MaxPublished
+                    ) rp
+                    where rp.PostNumber <= 3
+                    order by rp.FeedTitle, rp.Published desc
+                    for json path
+                )) as Posts
+            from rss.UserChannels uc
+            where uc.UserId = @UserId
+            order by uc.Name
+            for json path;
+            """,
+            new
+            {
+                this.UserId,
+                MinPublished = minPublished,
+                MaxPublished = maxPublished,
+            }) ?? [];
 
-        }
+        return Page();
     }
 
     public record PostSummary : PostBase
@@ -86,6 +87,6 @@ public class DailyModel : AppPageModel
 
     public record ChannelSummary : ChannelBase
     {
-        public IEnumerable<PostSummary> Posts { get; init; } = Enumerable.Empty<PostSummary>();
+        public IEnumerable<PostSummary> Posts { get; init; } = [];
     }
 }
