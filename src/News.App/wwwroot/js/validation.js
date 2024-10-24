b146e515ef7a8f269c20a9f3ad6405c879f917bb
Vladislav Borovikov <vborovikov@live.ru>
2024-10-24 19:54:07 +0500

    inlined variable

--- src/News.App/wwwroot/js/validation.js
+++ src/News.App/wwwroot/js/validation.js
@@ -1,12 +1,8 @@
-﻿//import { ValidationService } from 'aspnet-client-validation';
-
-const vs = new aspnetValidation.ValidationService();
+﻿const vs = new aspnetValidation.ValidationService();
 
 vs.addProvider("requiredif", function (value, element, params) {
     const otherElement = document.getElementsByName(params.property)[0];
-    const otherPropertyValue = otherElement.value;
-
-    if (otherPropertyValue === params.value) {
+    if (otherElement && otherElement.value === params.value) {
         const isValid = !!value;
         if (isValid && !!element.dataset.valRequiredifSkip) {
             // trigger other property validation if this one valid
