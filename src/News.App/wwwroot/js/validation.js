68ec6377039c3dded960be8c7bba86baf77c1039
Vladislav Borovikov <vborovikov@live.ru>
2024-10-24 19:11:40 +0500

    updated validation

---
+++ src/News.App/wwwroot/js/validation.js
@@ -0,0 +1,24 @@
+﻿//import { ValidationService } from 'aspnet-client-validation';
+
+const vs = new aspnetValidation.ValidationService();
+
+vs.addProvider("requiredif", function (value, element, params) {
+    const otherElement = document.getElementsByName(params.property)[0];
+    const otherPropertyValue = otherElement.value;
+
+    if (otherPropertyValue === params.value) {
+        const isValid = !!value;
+        if (isValid && !!element.dataset.valRequiredifSkip) {
+            // trigger other property validation if this one valid
+            otherElement.dataset.valRequiredifSkip = true;
+            vs.isFieldValid(otherElement, true);
+        }
+
+        element.dataset.valRequiredifSkip = false;
+        return isValid;
+    }
+
+    return true;
+});
+
+vs.bootstrap();
