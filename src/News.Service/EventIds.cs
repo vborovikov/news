a1d78294b630ae13de9fdc44cb3bc033258e8f8b
Vladislav Borovikov <vborovikov@live.ru>
2025-02-04 05:10:53 +0500

    updated event IDs

--- src/News.Service/EventIds.cs
+++ src/News.Service/EventIds.cs
@@ -2,20 +2,20 @@ namespace News.Service;
 
 public static class EventIds
 {
-    public static readonly EventId ServiceStarted = new(1, nameof(ServiceStarted));
-    public static readonly EventId ServiceStopped = new(2, nameof(ServiceStopped));
-    public static readonly EventId SchedulingStarted = new(3, nameof(SchedulingStarted));
-    public static readonly EventId SchedulingStopped = new(4, nameof(SchedulingStopped));
-    public static readonly EventId SchedulingFailed = new(5, nameof(SchedulingFailed));
+    public static readonly EventId ServiceStarted = new(100, nameof(ServiceStarted));
+    public static readonly EventId ServiceStopped = new(101, nameof(ServiceStopped));
+    public static readonly EventId SchedulingStarted = new(102, nameof(SchedulingStarted));
+    public static readonly EventId SchedulingStopped = new(103, nameof(SchedulingStopped));
+    public static readonly EventId SchedulingFailed = new(104, nameof(SchedulingFailed));
 
-    public static readonly EventId FeedUpdateInitiated = new(10, nameof(FeedUpdateInitiated));
-    public static readonly EventId FeedUpdateCompleted = new(11, nameof(FeedUpdateCompleted));
-    public static readonly EventId FeedUpdateSkipped = new(11, nameof(FeedUpdateSkipped));
-    public static readonly EventId FeedUpdateFailed = new(12, nameof(FeedUpdateFailed));
-    public static readonly EventId FeedUpdateScheduled = new(13, nameof(FeedUpdateScheduled));
-    public static readonly EventId FeedUpdateNotScheduled = new(14, nameof(FeedUpdateNotScheduled));
+    public static readonly EventId FeedUpdateInitiated = new(110, nameof(FeedUpdateInitiated));
+    public static readonly EventId FeedUpdateCompleted = new(111, nameof(FeedUpdateCompleted));
+    public static readonly EventId FeedUpdateSkipped = new(112, nameof(FeedUpdateSkipped));
+    public static readonly EventId FeedUpdateFailed = new(113, nameof(FeedUpdateFailed));
+    public static readonly EventId FeedUpdateScheduled = new(114, nameof(FeedUpdateScheduled));
+    public static readonly EventId FeedUpdateNotScheduled = new(115, nameof(FeedUpdateNotScheduled));
 
-    public static readonly EventId PostUpdateInitiated = new(20, nameof(PostUpdateInitiated));
-    public static readonly EventId PostUpdateCompleted = new(21, nameof(PostUpdateCompleted));
-    public static readonly EventId PostUpdateFailed = new(22, nameof(PostUpdateFailed));
+    public static readonly EventId PostUpdateInitiated = new(120, nameof(PostUpdateInitiated));
+    public static readonly EventId PostUpdateCompleted = new(121, nameof(PostUpdateCompleted));
+    public static readonly EventId PostUpdateFailed = new(122, nameof(PostUpdateFailed));
 }
