f6ffb084a17114c59b89433bbeb270d565a55c0b
Vladislav Borovikov <vborovikov@live.ru>
2025-03-31 15:20:09 +0500

    updated packages

--- src/News.Service/RequestHandler.cs
+++ src/News.Service/RequestHandler.cs
@@ -1,36 +1,22 @@
-﻿namespace News.Service
+﻿namespace News.Service;
+
+using Dodkin;
+using Dodkin.Dispatch;
+using Microsoft.Extensions.Logging;
+using Relay.RequestModel.Default;
+
+sealed class RequestHandler : QueueRequestHandler
 {
-    using System;
-    using Dodkin;
-    using Dodkin.Dispatch;
-    using Microsoft.Extensions.Logging;
-    using Relay.RequestModel.Default;
-
-    sealed class RequestHandler : QueueRequestHandler
+    public RequestHandler(Worker worker, MessageEndpoint endpoint, ILogger logger)
+        : base(DefaultRequestDispatcher.From(worker), endpoint, logger)
     {
-        public RequestHandler(Worker worker, MessageEndpoint endpoint, ILogger logger)
-            : base(new RequestDispatcher(worker), endpoint, logger)
-        {
-            this.PeekProperties = MessageProperty.CorrelationId;
-            this.ReadProperties = MessageProperty.None; // ignore request TTL
-            RecognizeTypesFrom(typeof(UpdateFeedCommand).Assembly);
-        }
-
-        // filter out the messages with CorrelationId set, these are the response messages for the UserAgent dispatcher
-        protected override bool CanDispatchRequest(in Message message) => message.CorrelationId != default;
-        // do nothing, the message will be picked up by the UserAgent dispatcher
-        protected override bool TryDispatchRequest(in Message message) => true;
-
-        private sealed class RequestDispatcher : DefaultRequestDispatcherBase
-        {
-            private readonly Worker worker;
-
-            public RequestDispatcher(Worker worker)
-            {
-                this.worker = worker;
-            }
-
-            protected override object GetRequestHandler(Type requestHandlerType) => this.worker;
-        }
+        this.PeekProperties = MessageProperty.CorrelationId;
+        this.ReadProperties = MessageProperty.None; // ignore request TTL
+        RecognizeTypesFrom(typeof(UpdateFeedCommand).Assembly);
     }
+
+    // filter out the messages with CorrelationId set, these are the response messages for the UserAgent dispatcher
+    protected override bool CanDispatchRequest(in Message message) => message.CorrelationId != default;
+    // do nothing, the message will be picked up by the UserAgent dispatcher
+    protected override bool TryDispatchRequest(in Message message) => true;
 }
