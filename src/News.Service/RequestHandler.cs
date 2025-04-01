8c1bd89664b8d9fe9809a0bf3b1ae436e0aa7428
Vladislav Borovikov <vborovikov@live.ru>
2025-04-01 11:42:16 +0500

    removed correlated message skipping

--- src/News.Service/RequestHandler.cs
+++ src/News.Service/RequestHandler.cs
@@ -10,13 +10,7 @@ sealed class RequestHandler : QueueRequestHandler
     public RequestHandler(Worker worker, MessageEndpoint endpoint, ILogger logger)
         : base(DefaultRequestDispatcher.From(worker), endpoint, logger)
     {
-        this.PeekProperties = MessageProperty.CorrelationId;
         this.ReadProperties = MessageProperty.None; // ignore request TTL
         RecognizeTypesFrom(typeof(UpdateFeedCommand).Assembly);
     }
-
-    // filter out the messages with CorrelationId set, these are the response messages for the UserAgent dispatcher
-    protected override bool CanDispatchRequest(in Message message) => message.CorrelationId != default;
-    // do nothing, the message will be picked up by the UserAgent dispatcher
-    protected override bool TryDispatchRequest(in Message message) => true;
 }
